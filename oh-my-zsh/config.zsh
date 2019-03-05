ce18df8c88596e7fae6584e25ebe66479dc6c566
Aslam Hadi Harsono <aslamhadi@gmail.com>
2019-03-05 06:08:28 +0000

    new zsh config

--- oh-my-zsh/config.zsh
+++ oh-my-zsh/config.zsh
@@ -1,38 +1,22 @@
-# Pyenv
-export PATH="/home/aslam/.pyenv/bin:$PATH"
-eval "$(pyenv init -)"
-eval "$(pyenv virtualenv-init -)"
+# Snap
+# export PATH="/snap/bin:$PATH"
+# export PATH="$HOME/.local/bin:$PATH"
+export PATH="$HOME/bin:$HOME/.local/bin:/snap/bin:$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
 
-# Pipenv
+# pipenv
 export PIPENV_VENV_IN_PROJECT=1
 
-# NVM
+eval "$(direnv hook zsh)"
+
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-# Postgres
-# export PATH="$PATH:/usr/lib/postgresql/9.6/bin"
+export TERM=xterm-256color
+export EDITOR='vim'
 
-# mssql
-export PATH="$PATH:/opt/mssql-tools/bin"
+# ZSH_THEME="terminalparty"
 
-# direnv
-eval "$(direnv hook zsh)"
+## set colors for LS_COLORS
+# eval `dircolors ~/.dircolors`
 
-# golang
-export PATH=$PATH:/usr/local/go/bin
-
-# yarn
-export PATH="$(yarn global bin):$PATH"
-
-# aliases
-alias kerja-pace="source /home/aslam/.pyenv/versions/pacescheduler/bin/activate"
-alias connect-webcert="psql -h wirausaha -U postgres -d WebCert" 
-
-# env variables
-export ASPNETCORE_ENVIRONMENT=Production
-
-# google cloud stuff
-export CLOUD_SDK_REPO="cloud-sdk-xenial"
-export PROJECT_ID="$(gcloud config get-value project -q)"
